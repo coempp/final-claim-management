356a94c1f41b912207484a172299e82c699f2157
surajbohrar <51718792+surajbohrar@users.noreply.github.com>
2021-08-10 14:33:15 +0530

    cOmmited

---
+++ Project/AuthorizationService/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user (userid,password,username) values ('root','password','admin');
+insert into user (userid,password,username) values ('Suraj','root','admin');
+insert into user (userid,password,username) values ('abc','root','admin');
+insert into user (userid,password,username) values ('name','password','admin');
